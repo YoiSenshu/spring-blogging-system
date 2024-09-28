27f66dc3c26ea4bc1c0e42bfde4ef63b2ac76ce0
Wojciech <wojteklyczba1@gmail.com>
2024-09-28 14:39:02 +0200

    Initial commit

---
+++ src/main/java/pl/yoisenshu/springbloggingsystem/SpringBloggingSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.yoisenshu.springbloggingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBloggingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBloggingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/yoisenshu/springbloggingsystem/SpringBloggingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.yoisenshu.springbloggingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBloggingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
